e8362221d38f4d7c5a207df154823c275572d2ac
Jonathon Storer <jonathon.scott.storer@gmail.com>
2015-08-24 14:43:54 -0400

    wip

--- test/passport-mock/strategy.test.js
+++ test/passport-mock/strategy.test.js
@@ -11,12 +11,12 @@ it('inherits from passport', function () {
 describe('init', function () {
   describe('name', function () {
     it('has a default', function () {
-      var strategy = new Strategy({ callbackURL: '/cb' }, function(){});
+      var strategy = Object.create(new Strategy({ callbackURL: '/cb' }, function(){}));
       expect(strategy.name).to.eql('mock')
     });
 
     it('can be set', function () {
-      var strategy = new Strategy({ name: 'test', callbackURL: '/cb' }, function(){});
+      var strategy = Object.create(new Strategy({ name: 'test', callbackURL: '/cb' }, function(){}));
       expect(strategy.name).to.eql('test')
     });
   });
@@ -37,7 +37,7 @@ describe('init', function () {
     });
 
     it('can set the callbackURL', function () {
-      var strategy = new Strategy({ callbackURL: '/here' }, function () {});
+      var strategy = Object.create(new Strategy({ callbackURL: '/here' }, function () {}));
       expect(strategy._callbackURL).to.eql('/here');
     });
   });
@@ -45,7 +45,7 @@ describe('init', function () {
   describe('#_factories', function () {
     var strategy;
     beforeEach(function(){
-      strategy = new Strategy({ callbackURL: '/' }, function () {});
+      strategy = Object.create(new Strategy({ callbackURL: '/' }, function () {}));
     });
 
     it('has factories', function () {
@@ -77,34 +77,39 @@ describe('#authenticate', function(){
 
   context('first call', function () {
     beforeEach(function(){
-      strategy = new Strategy({ callbackURL: '/cb' }, function() { });
+      strategy = Object.create(new Strategy({ callbackURL: '/cb' }, function() { }));
     });
 
     it('redirects the user to the callbackURL with the correct query param', function () {
-      strategy.redirect = function(path) { expect(path).to.eql('/cb?__mock_strategy_callback=true'); };
+      strategy.redirect = function(path) {
+        expect(path).to.eql('/cb?__mock_strategy_callback=true');
+      };
       strategy.authenticate(req, {});
     });
   });
 
   context('second call', function () {
     context('failure', function () {
-      beforeEach(function(){
-        strategy = new Strategy({ callbackURL: '/cb' }, function (at, rt, p, cb) { cb(); });
-        strategy.redirect = function () {};
-        strategy._error = new Error('test error');
-        strategy.authenticate(req, {});
-      });
+      describe('#_error', function () {
+        beforeEach(function(){
+          strategy = new Strategy({ callbackURL: '/cb' }, function (at, rt, p, cb) { cb(); });
+          strategy._error = new Error('test error');
+          strategy = Object.create(strategy);
+          strategy.redirect = function () {};
+          strategy.authenticate(req, {});
+        });
 
-      it('calls the fail method', function (done) {
-        strategy.fail = function(err, statusCode) {
-          expect(err).to.be.an.instanceOf(Error);
-          expect(err.message).to.eql('test error');
-          expect(statusCode).to.eql(401);
-          expect(strategy._error).to.not.exist;
-          done();
-        };
-        req.query.__mock_strategy_callback = true;
-        strategy.authenticate(req, {});
+        it('calls the fail method', function (done) {
+          strategy.fail = function(err, statusCode) {
+            expect(err).to.be.an.instanceOf(Error);
+            expect(err.message).to.eql('test error');
+            expect(statusCode).to.eql(401);
+            expect(strategy._error).to.not.exist;
+            done();
+          };
+          req.query.__mock_strategy_callback = true;
+          strategy.authenticate(req, {});
+        });
       });
     });
 
@@ -120,67 +125,79 @@ describe('#authenticate', function(){
         req.query.__mock_strategy_callback = true;
       });
 
-      it('creates an access token on the fly', function (done) {
-        expect(strategy._accessToken).to.not.exist;
-        strategy.success = function (profile) {
+      describe('#_accessToken', function () {
+        it('creates an access token on the fly', function (done) {
+          strategy = Object.create(strategy);
           expect(strategy._accessToken).to.not.exist;
-          expect(profile.accessToken).to.have.length(40);
-          done();
-        };
-        strategy.authenticate(req, {});
+          strategy.success = function (profile) {
+            expect(strategy._accessToken).to.not.exist;
+            expect(profile.accessToken).to.have.length(40);
+            done();
+          };
+          strategy.authenticate(req, {});
+        });
+
+        it('will use the provided accessToken', function (done) {
+          strategy._accessToken = 'my token';
+          strategy = Object.create(strategy);
+          strategy.success = function (profile) {
+            expect(strategy._accessToken).to.not.exist;
+            expect(profile.accessToken).to.eql('my token');
+            done();
+          };
+          strategy.authenticate(req, {});
+        });
       });
 
-      it('will use the provided accessToken', function (done) {
-        strategy._accessToken = 'my token';
-        strategy.success = function (profile) {
-          expect(strategy._accessToken).to.not.exist;
-          expect(profile.accessToken).to.eql('my token');
-          done();
-        };
-        strategy.authenticate(req, {});
-      });
-
-      it('creates a refresh token on the fly', function (done) {
-        expect(strategy._refreshToken).to.not.exist;
-        strategy.success = function (profile) {
+      describe('#_refreshToken', function () {
+        it('creates a refresh token on the fly', function (done) {
+          strategy = Object.create(strategy);
           expect(strategy._refreshToken).to.not.exist;
-          expect(profile.refreshToken).to.have.length(40);
-          done();
-        };
-        strategy.authenticate(req, {});
+          strategy.success = function (profile) {
+            expect(strategy._refreshToken).to.not.exist;
+            expect(profile.refreshToken).to.have.length(40);
+            done();
+          };
+          strategy.authenticate(req, {});
+        });
+
+        it('will use the provided refreshToken', function (done) {
+          strategy._refreshToken = 'my token';
+          strategy = Object.create(strategy);
+          strategy.success = function (profile) {
+            expect(strategy._refreshToken).to.not.exist;
+            expect(profile.refreshToken).to.eql('my token');
+            done();
+          };
+          strategy.authenticate(req, {});
+        });
       });
 
-      it('will use the provided refreshToken', function (done) {
-        strategy._refreshToken = 'my token';
-        strategy.success = function (profile) {
-          expect(strategy._refreshToken).to.not.exist;
-          expect(profile.refreshToken).to.eql('my token');
-          done();
-        };
-        strategy.authenticate(req, {});
-      });
+      describe('#_profile', function () {
+        it('handles the profile correctly', function (done) {
+          var tmpProfile = strategy._factory.build('Dropbox', { email: 'test@example.com', id: '1234 ' });
+          strategy._profile = tmpProfile;
+          strategy = Object.create(strategy);
+          strategy.success = function (profile) {
+            expect(strategy._profile).to.not.exist;
+            delete profile.accessToken;
+            delete profile.refreshToken;
+            expect(profile).to.eql(tmpProfile);
+            done();
+          };
+          strategy.authenticate(req, {});
+        });
 
-      it('handles the profile correctly', function (done) {
-        var tmpProfile = strategy._factory.build('Dropbox', { email: 'test@example.com', id: '1234 ' });
-        strategy._profile = tmpProfile;
-        strategy.success = function (profile) {
-          expect(strategy._profile).to.not.exist;
-          delete profile.accessToken;
-          delete profile.refreshToken;
-          expect(profile).to.eql(tmpProfile);
-          done();
-        };
-        strategy.authenticate(req, {});
-      });
-
-      it('will create a profile based on the name if one was not provided', function (done) {
-        strategy._factory.add('for-tests', function () { return 'hey!' });
-        strategy.name = 'for-tests';
-        strategy.success = function(profile) {
-          expect(profile).to.eql('hey!');
-          done();
-        };
-        strategy.authenticate(req, {});
+        it('will create a profile based on the name if one was not provided', function (done) {
+          strategy._factory.add('for-tests', function () { return 'hey!' });
+          strategy.name = 'for-tests';
+          strategy = Object.create(strategy);
+          strategy.success = function(profile) {
+            expect(profile).to.eql('hey!');
+            done();
+          };
+          strategy.authenticate(req, {});
+        });
       });
     });
   });
