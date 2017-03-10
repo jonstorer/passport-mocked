d39b00c3321ba0aad054ac20a091c78b578bf893
Douglas R. Lee <douglaslee@legalshieldcorp.com>
2017-03-10 16:27:47 -0600

    open id connect

--- test/strategy.test.js
+++ test/strategy.test.js
@@ -96,83 +96,7 @@ describe('#authenticate', function (){
       });
     });
 
-    context('when passReqToCallback is false', function () {
-      it('handles a verify method that asks for accessToken, refreshToken, and profile correctly', function (done) {
-        strategy = new Strategy({ callbackURL: '/cb' }, function (access_token, refresh_token, profile, cb) {
-          cb(null, {
-            profile: profile,
-            access_token: access_token,
-            refresh_token: refresh_token
-          });
-        });
-
-        strategy._token_response = { access_token: 'at', refresh_token: 'rt' };
-        strategy._profile = { id: 1 };
-        strategy = Object.create(strategy);
-
-        strategy.success = function (data) {
-          expect(data.access_token).to.eql('at');
-          expect(data.refresh_token).to.eql('rt');
-          expect(data.profile.id).to.eql(1);
-          done();
-        };
-        strategy.authenticate(req, {});
-      });
-
-      it('handles a verify method that asks for access token, refresh token, token response, and profile', function (done) {
-        strategy = new Strategy({ callbackURL: '/cb' }, function (access_token, refresh_token, token_response, profile, cb) {
-          cb(null, {
-            profile: profile,
-            token_response: token_response,
-            access_token: access_token,
-            refresh_token: refresh_token
-          });
-        });
-
-        strategy._token_response = { access_token: 'at', refresh_token: 'rt' };
-        strategy._profile = { id: 1 };
-        strategy = Object.create(strategy);
-
-        strategy.success = function (data) {
-          expect(data.access_token).to.eql('at');
-          expect(data.refresh_token).to.eql('rt');
-          expect(data.profile.id).to.eql(1);
-          expect(data.token_response).to.have.keys('access_token')
-          expect(data.token_response.access_token).to.eql('at');
-          expect(data.token_response.refresh_token).to.not.exist;
-          done();
-        };
-
-        strategy.authenticate(req, {});
-      });
-    });
-
-    context('when passReqToCallback is true', function () {
-      it('handles a verify method that asks for the request, access_token, refresh_token, and profile', function (done) {
-        strategy = new Strategy({ callbackURL: '/cb', passReqToCallback: true }, function (request, access_token, refresh_token, profile, cb) {
-          cb(null, {
-            request: request,
-            profile: profile,
-            access_token: access_token,
-            refresh_token: refresh_token
-          });
-        });
-
-        strategy._token_response = { access_token: 'at', refresh_token: 'rt' };
-        strategy._profile = { id: 1 };
-        strategy = Object.create(strategy);
-
-        strategy.success = function (data) {
-          expect(data.access_token).to.eql('at');
-          expect(data.refresh_token).to.eql('rt');
-          expect(data.profile.id).to.eql(1);
-          expect(data.request).to.eql(req);
-          done();
-        };
-
-        strategy.authenticate(req, {});
-      });
-
+    context('when the verify arity is 6', function () {
       it('handles a verify method that asks for request, access token, refresh token, token response, and profile', function (done) {
         strategy = new Strategy({ callbackURL: '/cb', passReqToCallback: true }, function (request, access_token, refresh_token, token_response, profile, cb) {
           cb(null, {
@@ -198,6 +122,106 @@ describe('#authenticate', function (){
           done();
         };
 
+        strategy.authenticate(req, {});
+      });
+    });
+
+    context('when the verify arity is 5', function () {
+      context('when passReqToCallback is false', function () {
+        it('handles a verify method that asks for access token, refresh token, token response, and profile', function (done) {
+          strategy = new Strategy({ callbackURL: '/cb' }, function (access_token, refresh_token, token_response, profile, cb) {
+            cb(null, {
+              profile: profile,
+              token_response: token_response,
+              access_token: access_token,
+              refresh_token: refresh_token
+            });
+          });
+
+          strategy._token_response = { access_token: 'at', refresh_token: 'rt' };
+          strategy._profile = { id: 1 };
+          strategy = Object.create(strategy);
+
+          strategy.success = function (data) {
+            expect(data.access_token).to.eql('at');
+            expect(data.refresh_token).to.eql('rt');
+            expect(data.profile.id).to.eql(1);
+            expect(data.token_response).to.have.keys('access_token')
+            expect(data.token_response.access_token).to.eql('at');
+            expect(data.token_response.refresh_token).to.not.exist;
+            done();
+          };
+
+          strategy.authenticate(req, {});
+        });
+      });
+
+      context('when passReqToCallback is true', function () {
+        it('handles a verify method that asks for the request, access_token, refresh_token, and profile', function (done) {
+          strategy = new Strategy({ callbackURL: '/cb', passReqToCallback: true }, function (request, access_token, refresh_token, profile, cb) {
+            cb(null, {
+              request: request,
+              profile: profile,
+              access_token: access_token,
+              refresh_token: refresh_token
+            });
+          });
+
+          strategy._token_response = { access_token: 'at', refresh_token: 'rt' };
+          strategy._profile = { id: 1 };
+          strategy = Object.create(strategy);
+
+          strategy.success = function (data) {
+            expect(data.access_token).to.eql('at');
+            expect(data.refresh_token).to.eql('rt');
+            expect(data.profile.id).to.eql(1);
+            expect(data.request).to.eql(req);
+            done();
+          };
+
+          strategy.authenticate(req, {});
+        });
+
+      });
+    });
+
+    context('when the verify arity is 4', function () {
+      it('handles a verify method that asks for accessToken, refreshToken, and profile correctly', function (done) {
+        strategy = new Strategy({ callbackURL: '/cb' }, function (access_token, refresh_token, profile, cb) {
+          cb(null, {
+            profile: profile,
+            access_token: access_token,
+            refresh_token: refresh_token
+          });
+        });
+
+        strategy._token_response = { access_token: 'at', refresh_token: 'rt' };
+        strategy._profile = { id: 1 };
+        strategy = Object.create(strategy);
+
+        strategy.success = function (data) {
+          expect(data.access_token).to.eql('at');
+          expect(data.refresh_token).to.eql('rt');
+          expect(data.profile.id).to.eql(1);
+          done();
+        };
+        strategy.authenticate(req, {});
+      });
+    });
+
+    context('when the verify arity is 2', function () {
+      it('handles a verify method that asks for tokenResponse correctly', function (done) {
+        strategy = new Strategy({ callbackURL: '/cb' }, function (token_response, cb) {
+          cb(null, { token_response: token_response });
+        });
+
+        strategy._token_response = { what: 'ever' };
+        strategy = Object.create(strategy);
+
+        strategy.success = function (data) {
+          expect(data.token_response).to.eql({ what: 'ever' });
+          done();
+        };
         strategy.authenticate(req, {});
       });
     });
